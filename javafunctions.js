6ab2188ae4650e3ce04e81ffbf6cc22f762a5f28
Nicholas Lockington <nick.lockington@outlook.com>
2025-03-03 15:21:55 +0000

    improved list method for tasks

--- javafunctions.js
+++ javafunctions.js
@@ -1,11 +1,25 @@
+const taskList = []
+let taskCount = 0
+function logTask() {
+    let taskInput = document.getElementById("taskName"); // Get the input element
+    let taskValue = taskInput.value; // get the value from the input field
+    
+    if (taskValue.trim() === "") { 
+        console.log("No task entered."); // debugging: Check if input is empty
+        return; // Stop execution if no task is entered
+    }
 
-function addtask(){
-  const task_list = [];
-  let task_name = document.getElementById("T_NAME").value;
-  let task_priority = document.getElementById("T_PRIORITY").value;
-  let task = task_priority +") " + task_name;
-  task_list.push(task);
-  task_list.sort();
-   document.getElementById("T_DISPLAY").textContent += task_list.join('\n');
-  return task
+    let duplicateTask = taskList.indexOf(taskValue)
+
+    if (duplicateTask >= 0){
+        console.log("this task exists already")
+    }
+    else{
+        console.log("Task added:", taskValue); // Should now correctly log the task name
+        taskList.push(taskValue);
+        let tasks = document.createElement("p");
+        tasks.innerHTML = String(taskList[taskCount]);
+        taskCount += 1;
+        document.getElementById("displayTasks").appendChild(tasks);
+    }
 }
