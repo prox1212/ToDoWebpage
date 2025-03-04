fec075f78f3a19250f40c44ed8496d7c909a4dc5
NickL <nick.lockington@outlook.com>
2025-03-04 23:24:28 +0000

    changed array to a dictionary which fixed the undefined task bug for re entered tasks

--- javafunctions.js
+++ javafunctions.js
@@ -1,4 +1,5 @@
-const taskList = []
+// creating a task
+const taskList = {}
 let taskCount = 0
 function logTask() {
     let taskInput = document.getElementById("taskName"); // get the input element
@@ -9,49 +10,47 @@ function logTask() {
         return; // stop execution if no task is entered
     }
 
-    let duplicateTask = taskList.indexOf(taskValue);
 
-    if (duplicateTask >= 0){
-        console.log("this task exists already"); // stops multiple tasks of the same name being created
+    for (const key in taskList) {
+        if (taskList[key].name === taskValue) {
+          console.log("This task already exists"); // loop to find duplicate task names
+        }
     }
-    else{
-        console.log("Task added:", taskValue); // log the task name
-        taskList.push(taskValue);
 
-        // create div for task
-        let div = document.createElement("div");
-        div.setAttribute("id", ("task" + (taskCount)));
-        div.setAttribute("class", ("taskItem"));
-        document.getElementById("displayTasks").appendChild(div);
+    console.log("Task added:", taskValue); // log the task name
 
-        // task details
-        let tasks = document.createElement("p"); // create a new paragraph element for the task
-        tasks.setAttribute("id", ("task" + (taskCount))); // gives each task its own unique id
-        tasks.innerHTML = String(taskList[taskCount]);
-        document.getElementById("task" + (taskCount)).appendChild(tasks);
+    // create div for task
+    let div = document.createElement("div");
+    div.setAttribute("id", ("task" + (taskCount)));
+    div.setAttribute("class", ("taskItem"));
+    document.getElementById("displayTasks").appendChild(div);
+    taskList["task" + taskCount] = ({id: "task" + taskCount, name: taskValue });
 
-        // delete button
-        let deleteBtn = document.createElement("button"); // create a button so that the task can be eddited (deleted)
-        deleteBtn.setAttribute("id", ("task" + (taskCount))); // sets the delete button to the same id as its corresponding task
-        deleteBtn.setAttribute("onclick", ("deleteTask(this.id)")) // using the this.id parameter to pass the id of the button that was clicked
-        deleteBtn.innerHTML = ("Delete");
-        document.getElementById("task" + (taskCount)).appendChild(deleteBtn);
+    // task details
+    let tasks = document.createElement("p"); // create a new paragraph element for the task
+    tasks.setAttribute("id", ("task" + (taskCount))); // gives each task its own unique id
+    const taskID = "task" + taskCount; // creates id
+    const taskName = taskList[taskID].name; // gets the name of an object with the specific id
+    tasks.innerHTML = String(taskName);
+    document.getElementById("task" + (taskCount)).appendChild(tasks);
 
-        taskCount += 1;
-    }
+    // delete button
+    let deleteBtn = document.createElement("button"); // create a button so that the task can be eddited (deleted)
+    deleteBtn.setAttribute("id", ("task" + (taskCount))); // sets the delete button to the same id as its corresponding task
+    deleteBtn.setAttribute("onclick", ("deleteTask(this.id)")) // using the this.id parameter to pass the id of the button that was clicked
+    deleteBtn.innerHTML = ("Delete");
+    document.getElementById("task" + (taskCount)).appendChild(deleteBtn);
+    taskCount += 1;
 }
 
+// deleting a task
 function deleteTask(clicked_id) {
-    console.log("task deleted: " + (clicked_id));
+    console.log("task deleted: " + (clicked_id)); // debugging
 
-    let task = document.getElementById(clicked_id);
-    let taskName = task.value;
+    delete taskList[clicked_id] // deletes the record with the specific id (one where the button was clicked)
 
-    let taskLocation = taskList.indexOf(taskName);
-    taskList.splice(taskLocation, 1);
-
-    console.log(taskList)
+    console.log(taskList) // debugging, checking if its empty
 
     const element = document.getElementById(clicked_id);
-    element.remove();
+    element.remove(); // removes the elements from the page so that the user doesnt see the task anymore
 }
